61cf64c6adb269e9c1280c063b016c9ad13fd9f3
dave <wenqiweiabcd@gmail.com>
2018-01-24 17:06:53 +1100

    added matrix weight blockram unit

--- zynq/neuralnet_ip/neuralnet_ip.srcs/sources_1/new/weight_memory.vhd
+++ zynq/neuralnet_ip/neuralnet_ip.srcs/sources_1/new/weight_memory.vhd
@@ -44,39 +44,100 @@ architecture Behavioral of weight_memory is
         := integer (ceil (real(ncols * nrows)/real(4)));
     type memarray_t is array (nwords - 1 downto 0) of
         std_logic_vector (63 downto 0);
+    signal word_ptr: integer range 3 downto 0;
     signal read_ptr: integer  range nwords - 1 downto 0;
     signal read_enable: std_logic;
     signal bram_out: std_logic_vector (63 downto 0);
     signal write_ptr: integer range nwords - 1 downto 0;
     signal memarray: memarray_t;
+    --input pipeline registers
+    signal validin_delayed: std_logic;
+    signal datain_delayed: std_logic_vector (63 downto 0);
+    --output pipeline registers
+    signal sig_validout: std_logic;
+    signal sig_dataout: std_logic_vector (15 downto 0);
+    signal validout_delayed: std_logic;
+    signal dataout_dalayed: std_logic_vector (15 downto 0);
 begin
+    -- put everything here between pipeline registers
+    -- to improve block ram performance
+    io_delay:
+    process (clk, alrst) is
+    begin
+        if (rising_edge(clk)) then
+            if (alrst = '0') then
+                validin_delayed <= '0';
+                datain_delayed <= (others => '0');
+                validout_delayed <= '0';
+                dataout_delayed <= (others => '0');
+            else
+                validin_delayed <= validin;
+                datain_delayed <= datain;
+                validout_delayed <= sig_validout;
+                dataout_delayed <= sig_dataout;
+            end if;
+        end if;
+    end process;
 
     blockram_proc:
     process (clk, alrst) is
     begin
-        if (clk'event and clk = '1') then
-            if (validin = '1') then
-                memarray (write_ptr) <= datain;
-            end if;
+        if (rising_edge(clk)) then
+            if (alrst = '0') then
+                for I in nwords - 1 downto 0 loop
+                    memarray (I) <= (others => '0');
+                end loop;
+            else
+                if (validin_delayed = '1') then
+                    memarray (write_ptr) <= datain;
+                end if;
 
-            if (read_enable = '1') then
-                bram_out <= memarray (read_ptr);
+                if (read_enable = '1') then
+                    bram_out <= memarray (read_ptr);
+                    sig_validout <= '1';
+                else
+                    sig_validout <= '0'; 
+                end if;
             end if;
         end if;
     end process;
     
-    write_proc:
+    rwaddr_proc:
     process (clk, alrst) is
     begin
-        if (clk'event and clk = '1') then
-            if (validin = '1') then
-                write_ptr <= write_ptr + 1;
-            elsif (alrst = '0') then
+        if (rising_edge(clk)) then
+            if (alrst = '0') then
                 write_ptr <= 0;
+                read_ptr <= 0;
+            else
+                if (validin_delayed = '1') then
+                    write_ptr <= (write_ptr + 1) mod nwords;
+                end if;
+
+                if (read_enable = '1') then
+                    read_ptr <= (read_ptr + 1) mod nwords;
+                end if;
+
             end if;
         end if;
     end process;
 
+--word pointer incrementing process
+    wordptr_proc:
+    process (clk, alrst) is
+    begin
+        if (rising_edge(clk)) then
+            if (alrst = '0') then
+                word_ptr <= 0;
+            else
+                word_ptr <= (word_ptr + 1) mod 4;
+            end if;
+        end if;
+    end process;
+    read_enable <= '1' when word_ptr = 3 else '0';
+    sig_dataout <= bram_out ((word_ptr + 1)* 16 - 1 downto word_ptr * 16);
 
+    dataout <= dataout_delayed;
+    validout <= validout_delayed;
 
 end Behavioral;
