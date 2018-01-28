57ec2e9781e41ce769ac08b3f1b44f07063f46b6
dave <wenqiweiabcd@gmail.com>
2018-01-28 22:32:25 +1100

    Added matrix output stage shift register, added helper package

---
+++ zynq/neuralnet_ip/neuralnet_ip.srcs/sources_1/new/helperpkg.vhd
@@ -0,0 +1,31 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 28.01.2018 21:09:08
+-- Design Name: 
+-- Module Name: helperpkg - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+package helperpkg is
+    type wordarr_t is array (integer range <>) of std_logic_vector (15 downto 0);
+end package;
+
+package body helperpkg is
+
+end package body;
---
+++ zynq/neuralnet_ip/neuralnet_ip.srcs/sources_1/new/vector_shifter.vhd
@@ -0,0 +1,93 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 28.01.2018 21:02:53
+-- Design Name: 
+-- Module Name: vector_shifter - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.helperpkg.all;
+
+-- Pipelined serializer
+-- Serializes the parrallel output from row processing elements
+-- Fill to the brim, start flushing while filling with the next vector
+entity vector_shifter is
+generic(
+    nrows: integer := 100
+);
+port (
+    clk: in std_logic;
+    alrst: in std_logic;
+-- parallel input
+    array_in: in wordarr_t (nrows - 1 downto 0);
+    valid_in: in std_logic_vector (nrows - 1 downto 0); 
+-- serial output
+    valid_out: out std_logic;
+    activated_out: out std_logic_vector (15 downto 0);
+    unactivated_out: out std_logic_vector (15 downto 0)
+);
+end vector_shifter;
+
+architecture Behavioral of vector_shifter is
+    
+    -- extra bit of valid flag
+    type register_array_t is array (nrows - 1 downto 0) of std_logic_vector (16 downto 0);
+    
+    signal registers: register_array_t (nrows - 1 downto 0);
+    -- multiplx the input signals to the registers
+    -- between shifting and accepting input from matrix
+    signal data_signals: register_array_t (nrows - 1 downto 0);
+    signal sel_signals: std_logic_vector (nrows - 1 downto 0);
+    signal data_shift: register_array_t (nrows - 1 downto 0);
+    signal data_input: register_array_t (nrows - 1 downto 0);
+
+begin
+
+for I in nrows - 1 downto 1 generate
+    data_shift (I) <= data_shift (I - 1);
+end generate;
+data_shift (0) <= (others => '0');
+
+for I in nrows - 1 downto 0 generate
+    data_input (I) (16) <= valid_in (I);
+    data_input (I) (15 downto 0) <= array_in (I); 
+end generate;
+
+for I in nrows - 1 downto 0 generate
+    data_signals (I) <= data_shift(I) when sel_signals(I) = '1' else data_input(I);
+end generate;
+
+latching_proc:
+process (clk, alrst) is
+begin
+    if (rising_edge(clk)) then
+        if (alrst = '0') then
+            for I in nrows - 1 downto 0 loop
+                registers(I) <= (others => '0');
+            end loop;
+        else
+            registers(I) <= data_signals(I);
+        end if;
+    end if;
+end process;
+
+--TODO:
+-- wire output to sigmoid unit
+-- mux selection bit logic
+
+end Behavioral;
