f12c833cd98882ed0ad76575c33e0239e42d0502
dave <wenqiweiabcd@gmail.com>
2018-02-03 17:23:47 +1100

    added pseudo random number generator and l1 cache

---
+++ zynq/neuralnet_ip/neuralnet_ip.srcs/sources_1/new/lfsr.vhd
@@ -0,0 +1,58 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03.02.2018 16:44:19
+-- Design Name: 
+-- Module Name: lfsr - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- linear feedback shift register
+-- use as a coarse-grain random number generator
+entity lfsr is
+port(
+    clk: in std_logic;
+    alrst: in std_logic;
+    pseudornd: out std_logic_vector (15 downto 0)
+);
+end lfsr;
+
+architecture Behavioral of lfsr is
+-- use bits 15, 13  and 1 as XOR gate input
+    signal shiftreg: std_logic_vector (15 downto 0);
+    signal shiftin: std_logic;
+    signal shiftreg_next: std_logic_vector (15 downto 0);
+begin
+    shiftin <= ((not shiftreg(15)) xor shiftreg(13) xor shiftreg(1));
+    shiftreg_proc:
+    process(clk, alrst) is
+    begin
+        if (rising_edge(clk)) then
+            if (alrst = '0') then
+                shiftreg <= (others => '0');
+            else
+                shiftreg(15) <= shiftin;
+                for I in 14 downto 0 loop
+                    shiftreg(I) <= shiftreg (I + 1);
+                end loop;
+            end if;
+        end if;
+    end process;
+    pseudornd <= shiftreg;
+
+end Behavioral;
