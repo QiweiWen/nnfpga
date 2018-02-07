0b8b8eb3a143a6f44f0567bae7ceec413c56592f
dave <wenqiweiabcd@gmail.com>
2018-02-07 22:27:58 +1100

    Finished writing the module that forwards data from l2 cache to l1 cache not tested

--- zynq/neuralnet_ip/neuralnet_ip.srcs/sources_1/new/param_feeder.vhd
+++ zynq/neuralnet_ip/neuralnet_ip.srcs/sources_1/new/param_feeder.vhd
@@ -1,29 +1,10 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 06.02.2018 21:25:46
--- Design Name: 
--- Module Name: param_feeder - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity param_feeder is
-generic map(
+generic(
     ncols: integer := 10
 );
 port (
@@ -40,6 +21,8 @@ port (
     dataout: out std_logic_vector (15 downto 0);
     validout: out std_logic;
 -- flag passed to the last row feeder
+-- use this signal from the top most unit to
+-- drive reset
     full : out std_logic;
 -- flag passed from the next row feeder
     slave_full: in std_logic
@@ -50,12 +33,75 @@ architecture Behavioral of param_feeder is
 
 -- finished filling the cache of this row processor
 signal sig_full: std_logic;
--- how many words we've forwarded to the row processor
-signal nwords_written: integer range ncols downto 0;
--- whether new data is forwarded downwards or into the row processor
-signal keep_datain: std_logic;
+-- how many words we've written to the row processor
+signal nwords_written: integer range ncols - 1 downto 0;
+-- latched signals to be forwarded next cycle
+signal validin_pipeline: std_logic;
+signal datain_pipeline: std_logic_vector (15 downto 0);
+
+-- forwarding: send to next unit
+-- writing: send to row processor
+type state_t is (forwarding, writing);
+signal this_state: state_t;
+signal next_state: state_t;
 
 begin
 
+dataout <= datain when this_state = writing else (others => '0');
+validout <= validin when this_state = writing else '0';
+
+pipeline_proc:
+process (clk) is
+begin
+    if (rising_edge(clk)) then
+        if (alrst = '0') then
+            datain_pipeline <= (others => '0'); 
+            validin_pipeline <= '0';
+        elsif (this_state = forwarding) then
+            datain_pipeline <= datain; 
+            validin_pipeline <= validin;
+        end if;
+    end if;
+end process;
+
+datafwd <= datain_pipeline;
+validfwd <= validin_pipeline;
+
+-- set up for the next cycle where the last chunk of our data
+-- will be forwarded
+sig_full <= '1' when validin = '1' and nwords_written = ncols - 1 else '0';
+full <= sig_full;
+
+nwritten_proc:
+process (clk) is
+begin
+    if (rising_edge (clk)) then
+        if (alrst = '0') then
+            nwords_written <= 0;
+        elsif (validin = '1' and this_state = writing) then
+            nwords_written <= (nwords_written + 1) mod ncols;
+        end if;
+    end if;
+end process;
+
+next_state_logic:
+process (sig_full, slave_full, this_state, alrst) is
+begin
+    if (slave_full = '1') then
+        next_state <= writing;
+    elsif (alrst = '0' or sig_full = '1') then
+        next_state <= forwarding;
+    else
+        next_state <= this_state;
+    end if;
+end process;
+
+state_transition:
+process (clk) is
+begin
+    if (rising_edge(clk)) then
+        this_state <= next_state;
+    end if;
+end process;
 
 end Behavioral;
