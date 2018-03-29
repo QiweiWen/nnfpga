d766640218a8b4e6b02490b5a974d3fc1cbc81c5
dave <wenqiweiabcd@gmail.com>
2018-03-29 15:55:46 +1100

    tested sipo feeder

--- zynq/neuralnet_ip/neuralnet_ip.srcs/sources_1/new/param_sipo.vhd
+++ zynq/neuralnet_ip/neuralnet_ip.srcs/sources_1/new/param_sipo.vhd
@@ -20,14 +20,15 @@ end param_sipo;
 
 architecture Behavioral of param_sipo is
 -- main shift register
-signal shift_register: wordarr_t (nrows - 1 downto 0);
+signal shift_register: wordarr_t (nrows - 2 downto 0);
 -- row count, global valid flag
 signal row_count     : integer range nrows - 1 downto 0;
 signal all_rows_valid: std_logic;
 
 begin
 -- output wiring
-arrayout <= shift_register;
+arrayout (nrows - 2 downto 0) <= shift_register;
+arrayout (nrows - 1)          <= datain;
 validout <= (others => '1') when all_rows_valid = '1' else (others => '0');
 
 -- count the number of rows that are ready
@@ -44,14 +45,20 @@ begin
 end process;
 
 -- main shift register process
-shift_register(nrows - 1) <= datain;
 shiftproc:
 process (clk, alrst) is
 begin
     if (rising_edge (clk)) then
-        for I in nrows - 2 downto 0 loop
-            shift_register (I) <= shift_register (I + 1);
-        end loop;
+        if (alrst = '0') then
+            for I in nrows - 2 downto 0 loop
+                shift_register (I) <= (others => '0');
+            end loop;
+        else
+            for I in nrows - 3 downto 0 loop
+                shift_register (I) <= shift_register (I + 1);
+            end loop;
+            shift_register (nrows - 2) <= datain;
+        end if;
     end if;
 end process;
 
