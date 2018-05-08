a6d0bd2e46869050b3a30dd5ac5974112c9940ac
dave <wenqiweiabcd@gmail.com>
2018-05-08 22:36:56 +1000

    changed to fwft

--- zynq/neuralnet_ip/neuralnet_ip.srcs/sources_1/imports/oldhdl/stolen_fifo.vhd
+++ zynq/neuralnet_ip/neuralnet_ip.srcs/sources_1/imports/oldhdl/stolen_fifo.vhd
@@ -1,12 +1,10 @@
--- stolen from deathbylogic.com
-
 library ieee;
-use ieee.std_logic_1164.all;
+use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
-
-entity std_fifo is
+ 
+entity fwft_fifo is
 	generic (
-		constant data_width  : positive := 8;
+		constant data_width  : positive := 16;
 		constant fifo_depth	: positive := 256
 	);
 	port ( 
@@ -16,32 +14,20 @@ entity std_fifo is
 		datain	: in  std_logic_vector (data_width - 1 downto 0);
 		readen	: in  std_logic;
 		dataout	: out std_logic_vector (data_width - 1 downto 0);
-        validout: out std_logic;
+                ackout  : out std_logic;
 		empty	: out std_logic;
 		full	: out std_logic
 	);
-end std_fifo;
+end fwft_fifo;
+ 
+architecture behavioral of fwft_fifo is
 
-architecture behavioral of std_fifo is
-
-    signal validout_next: std_logic;
+    signal validout: std_logic;
     signal sig_empty: std_logic;
-
 begin
-    -- delay readen and empty for one cycle to drive output valid flag
-    validout_next <= '1' when readen = '1' and sig_empty = '0' else '0';
-    vout_proc: process (clk, rst)
-    begin
-        if (rising_edge(clk)) then
-            if (rst = '0') then
-                validout <= '0';
-            else
-                validout <= validout_next;
-            end if;
-        end if;
-    end process;
-    
-    empty <= sig_empty;
+        validout <= '1' when sig_empty = '0' else '0';
+        ackout   <= '1' when validout = '1' and readen = '1' else '0';
+        empty <= sig_empty; 
 	-- memory pointer process
 	fifo_proc : process (clk)
 		type fifo_memory is array (0 to fifo_depth - 1) of std_logic_vector (data_width - 1 downto 0);
@@ -60,13 +46,10 @@ begin
 				looped := false;
 				
 				full  <= '0';
-				sig_empty <=  '1';
+				sig_empty <= '1';
 			else
 				if (readen = '1') then
 					if ((looped = true) or (head /= tail)) then
-						-- update data output
-						dataout <= memory(tail);
-						
 						-- update tail pointer as needed
 						if (tail = fifo_depth - 1) then
 							tail := 0;
@@ -75,8 +58,6 @@ begin
 						else
 							tail := tail + 1;
 						end if;
-						
-						
 					end if;
 				end if;
 				
@@ -96,19 +77,22 @@ begin
 					end if;
 				end if;
 				
-				-- update empty and full flags
+				-- update data output
+				dataout <= memory(tail);
+				
+				-- update sig_empty and full flags
 				if (head = tail) then
 					if looped then
 						full <= '1';
 					else
-						sig_empty <=  '1';
+						sig_empty <= '1';
 					end if;
 				else
-					sig_empty <=  '0';
+					sig_empty	<= '0';
 					full	<= '0';
 				end if;
 			end if;
 		end if;
 	end process;
-		
-end Behavioral;
+	
+end behavioral;
